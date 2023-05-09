83321818197949c159f9673595c9acd7ba418b00
0xdineshkumarsm <105421120+0xdineshkumarsm@users.noreply.github.com>
2023-05-09 10:57:05 +0530

    added public input

--- src/example1.rs
+++ src/example1.rs
@@ -15,6 +15,7 @@ struct ACell<F:FieldExt>(AssignedCell<F,F>);
 struct FiboConfig{
     pub advice: [Column<Advice>; 3],
     pub selector: Selector,
+    pub instance: Column<Instance>,
 }
 
 struct FiboChip<F: FieldExt>{
@@ -30,6 +31,7 @@ impl <F: FieldExt> FiboChip<F>{
     fn configure(
         meta: &mut ConstraintSystem<F>,
         advice: [Column<Advice>; 3],
+        instance: Column<Instance>
     ) -> FiboConfig{
         let (col_a,col_b,col_c) = (advice[0],advice[1],advice[2]);
         let selector = meta.selector();
@@ -37,6 +39,7 @@ impl <F: FieldExt> FiboChip<F>{
         meta.enable_equality(col_a);
         meta.enable_equality(col_b);
         meta.enable_equality(col_c);
+        meta.enable_equality(instance);
 
         meta.create_gate("add", |meta| {
             let s = meta.query_selector(selector);
@@ -50,6 +53,7 @@ impl <F: FieldExt> FiboChip<F>{
         FiboConfig { 
             advice: [col_a, col_b, col_c], 
             selector,
+            instance
         }
 
     }
@@ -91,28 +95,33 @@ impl <F: FieldExt> FiboChip<F>{
     }
 
     fn assign_row(&self,mut layouter: impl Layouter<F>, prev_b:&ACell<F>, prev_c:&ACell<F>)
-        -> Result<ACell<F>,Error>{
-            layouter.assign_region(
-                || "next row",
-                |mut region|{
-                    self.config.selector.enable(&mut region, 0)?;
-                    prev_b.0.copy_advice(|| "a",&mut region, self.config.advice[0], 0)?;
-                    prev_c.0.copy_advice(|| "b",&mut region, self.config.advice[1], 0)?;
+    -> Result<ACell<F>,Error>{
+        layouter.assign_region(
+            || "next row",
+            |mut region|{
+                self.config.selector.enable(&mut region, 0)?;
+                prev_b.0.copy_advice(|| "a",&mut region, self.config.advice[0], 0)?;
+                prev_c.0.copy_advice(|| "b",&mut region, self.config.advice[1], 0)?;
 
-                    let c_val = prev_b.0.value().and_then(
-                        |b| {
-                            prev_c.0.value().map(|c| *b + *c )
-                        }
-                    );
+                let c_val = prev_b.0.value().and_then(
+                    |b| {
+                        prev_c.0.value().map(|c| *b + *c )
+                    }
+                );
 
-                    let c_cell = region.assign_advice(
-                        || "c", self.config.advice[2], 0, || c_val.ok_or(Error::Synthesis),
-                    ).map(ACell)?;
+                let c_cell = region.assign_advice(
+                    || "c", self.config.advice[2], 0, || c_val.ok_or(Error::Synthesis),
+                ).map(ACell)?;
 
-                    Ok(c_cell)
-                }
-            )
-        }
+                Ok(c_cell)
+            }
+        )
+    }
+
+    pub fn expose_public(&self,mut layouter: impl Layouter<F>,cell: &ACell<F>,row:usize)-> Result<(), Error>{
+        layouter.constrain_instance(cell.0.cell(), self.config.instance, row)
+    }
+
 }
 
 #[derive(Default)]
@@ -133,17 +142,21 @@ impl <F: FieldExt> Circuit<F> for MyCircuit<F> {
         let col_a = meta.advice_column();
         let col_b = meta.advice_column();
         let col_c = meta.advice_column();
+        let instance = meta.instance_column();
 
-        FiboChip::configure(meta,[col_a,col_b,col_c])
+        FiboChip::configure(meta,[col_a,col_b,col_c],instance)
     }
 
     fn synthesize(&self, config: Self::Config,mut layouter: impl Layouter<F>) -> Result<(), Error> {
         let chip = FiboChip::construct(config);
 
-        let (_,mut prev_b,mut prev_c) = chip.assign_first_row(
+        let (prev_a,mut prev_b,mut prev_c) = chip.assign_first_row(
             layouter.namespace(|| "first row"),
             self.a,self.b,
         )?;
+
+        chip.expose_public(layouter.namespace(|| "private a"), &prev_a, 0)?;
+        chip.expose_public(layouter.namespace(|| "private b"), &prev_a, 1)?;
 
         for _i in 3..10 {
             let c_cell = chip.assign_row(
@@ -156,6 +169,8 @@ impl <F: FieldExt> Circuit<F> for MyCircuit<F> {
 
 
         }
+
+        chip.expose_public(layouter.namespace(|| "out"), &prev_c, 2)?;
 
 
 
@@ -170,11 +185,19 @@ fn main() {
     let a = Fp::from(1);
     let b = Fp::from(1);
 
+    let out = Fp::from(55);
+
     let circuit = MyCircuit{
         a: Some(a),
         b: Some(b),
     };
 
-    let prover = MockProver::run(k,&circuit,vec![]).unwrap();
+    let mut public_input = vec![a,b,out];
+
+    let prover = MockProver::run(k,&circuit,vec![public_input.clone()]).unwrap();
     prover.assert_satisfied();
+
+    public_input[2] += Fp::one();
+    // let prover = MockProver::run(k,&circuit,vec![public_input.clone()]).unwrap();
+    // prover.assert_satisfied();
 }
